61b05309ec22cebbfb25bb836b2dcdc3fbfe064a
Vijayasri Iyer <thisisvij98@gmail.com>
2018-07-01 14:10:12 +0530

    Added world file path

--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xterm  -e  "source /opt/ros/kinetic/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  "export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/World/homebot_world.world; source /opt/ros/kinetic/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  "source /opt/ros/kinetic/setup.bash; roslaunch home_services_robot gmapping.launch" &
 sleep 5
