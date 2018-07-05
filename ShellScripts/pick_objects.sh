599fa58589ec7ca4917d3260178c3039cf3a8c96
Vijayasri Iyer <thisisvij98@gmail.com>
2018-07-05 15:39:21 +0530

    Added map file parameter

--- ShellScripts/pick_objects.sh
+++ ShellScripts/pick_objects.sh
@@ -2,7 +2,7 @@
 
 xterm -e "export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/World/homebot_world.world; source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/World/map.yaml; source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
